f0aa872e0e7c5ed7bba3493d220486f34743d183
Mohammadreza B <b.mohammadreza@gmail.com>
2023-08-11 14:22:38 -0400

    core_samples:graph - implemented is_cyclic() check. added more tests.

--- core_samples/graph/graph.py
+++ core_samples/graph/graph.py
@@ -15,9 +15,14 @@ class GraphStruct:
         self._nodes = nodes
 
     def traverse(self, t_type) -> None:
+        """ TODO: corner case: in case if there is a node
+         which is not connected to others, need to initiate
+         the traverse function on each node separately """
+
         if t_type == GraphStruct.T_TYPE_DFS:
-            return self._dfs(0, [])
-        return self._bfs(0)
+            self._dfs(0, [], [])
+        else:
+            self._bfs(0)
 
     def shortest_path(self) -> None:
         """implement both Dijkstra's andBellman-Ford's algorithms"""
@@ -27,20 +32,36 @@ class GraphStruct:
         """implement both Kruskal's and Prim's algorithms"""
         pass
 
-    def _dfs(self, node_index: int, visited: list[int]) -> None:
+    def _dfs(self, node_index: int, visited: list[int], stack: list[int]) -> bool:
+        is_cyclic: bool = False
+
+        if len(stack) > 2 and node_index != stack[-2] and node_index in stack:
+            is_cyclic = True
+            print(f'GraphStruct_dfs(): ^^stack cycle found: {stack}')
+
         if node_index in visited:
             print(f'GraphStruct_dfs(): node already visited - index: {node_index}')
-            return
+            return is_cyclic
 
         print(f'GraphStruct_dfs(): >>>>>>>>>>node traversed - index: {node_index}, data: {self._nodes[node_index]._data}')
         visited.append(node_index)
+
+        stack.append(node_index)
+        print(f'GraphStruct_dfs(): ^^stack push(): {stack}')
 
         adj_arr = self._adj_matrix[node_index]
         print(f'GraphStruct_dfs(): **********adjucent nodes for node-{node_index}: {adj_arr}')
 
         for index, val in enumerate(adj_arr):
             if val > 0:
-                self._dfs(index, visited=visited)
+                ret = self._dfs(index, visited=visited, stack=stack)
+                if ret == True:
+                    is_cyclic = True
+
+        stack.pop()
+        print(f'GraphStruct_dfs(): ^^stack pop(): {stack}')
+
+        return is_cyclic
 
     def _bfs(self, start_index: int) -> None:
         visited: list[int] = []
@@ -63,8 +84,9 @@ class GraphStruct:
                     print(f'GraphStruct_bfs(): node-{index} added into "t_visit" list')
 
 
+
     def is_cyclic(self) -> bool:
-        pass
+        return self._dfs(0, [], [])
 
     def remove(self, val) -> bool:
         pass
@@ -72,12 +94,38 @@ class GraphStruct:
     def _update_graph(self) -> None:
         pass
 
-def build_graph(adj_matrix: list) -> None:
-    pass
-
 if __name__ == '__main__':
-    graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
-                        , [[0,1,1,1], [1,0,1,0], [1,1,0,0], [1,0,0,0]])
+    # graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
+    #                     , [[0,1,1,1], [1,0,1,0], [1,1,0,0], [1,0,0,0]])
     
     # graph.traverse(GraphStruct.T_TYPE_DFS)
+    # graph.traverse(GraphStruct.T_TYPE_BFS)
+    # print(f'is graph cyclic: {graph.is_cyclic()}')
+
+    # graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
+    #                     , [[0,1,0,1], [1,0,1,0], [0,1,0,0], [1,0,0,0]])
+    # print(f'is graph cyclic: {graph.is_cyclic()}')
+
+    # graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
+    #                     , [[0,1,0,0], [1,0,1,0], [0,1,0,1], [0,0,1,0]])
+    # print(f'is graph cyclic: {graph.is_cyclic()}')
+
+    # graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
+    #                     , [[0,1,0,1], [1,0,1,0], [0,1,0,1], [1,0,1,0]])
+    # print(f'is graph cyclic: {graph.is_cyclic()}')
+    # graph.traverse(GraphStruct.T_TYPE_BFS)
+
+    # graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
+    #                     , [[0,1,1,1], [1,0,1,0], [1,1,0,1], [1,0,1,0]])
+    # print(f'is graph cyclic: {graph.is_cyclic()}')
+    # graph.traverse(GraphStruct.T_TYPE_BFS)
+
+    # graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
+    #                     , [[0,1,1,1], [1,0,1,1], [1,1,0,1], [1,1,1,0]])
+    # print(f'is graph cyclic: {graph.is_cyclic()}')
+    # graph.traverse(GraphStruct.T_TYPE_BFS)
+
+    graph = GraphStruct([GraphNode('aaa'), GraphNode('aab'), GraphNode('abb'), GraphNode('bbb')]
+                        , [[0,1,0,0], [1,0,1,0], [0,1,1,1], [0,0,1,0]])
+    # print(f'is graph cyclic: {graph.is_cyclic()}')
     graph.traverse(GraphStruct.T_TYPE_BFS)
