eea51adcd4beddc02b6e5957a5b9ef55392b94ec
Mohammadreza B <b.mohammadreza@gmail.com>
2023-09-22 16:09:46 -0400

    Implement Stack data structure. Add associated tests.

---
+++ core_samples/stack/stack.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from typing import Any
+
+class Stack:
+    def __init__(self) -> None:
+        self._container: list[Any] = []
+
+    def is_empty(self):
+        return len(self._container) == 0
+    
+    def push(self, item: Any) -> int:
+        """ Pushes the item into stack and
+          returns the index in which the item pushed """
+
+        self._container.append(item)
+        return len(self._container) - 1
+    
+    def peek(self) -> Any:
+        if self.is_empty():
+            return None
+
+        return self._container[-1]
+    
+    def pop(self) -> Any:
+        if self.is_empty():
+            return None
+
+        item = self._container.pop()
+        return item
+
+if __name__ == "__main__":
+    pass
---
+++ core_samples/stack/test/test_stack.py
@@ -0,0 +1,47 @@
+from .. import priority_queue
+import pytest
+import json
+import os
+
+from typing import Self,Any
+
+@pytest.fixture
+def get_file_list() -> list[str]:
+    return os.listdir('test/arrays')
+
+@pytest.fixture
+def get_array_list(get_file_list) -> list[(list[int], str)]:
+    arr_file_list = []
+
+    if len(get_file_list) < 1:
+        raise ValueError('get_array_list(): no test-case files found...')
+
+    for json_file in get_file_list: 
+        with open(f'test/arrays/{json_file}', 'r') as fstr:
+            file_data = json.load(fstr)
+
+        try:
+            arr = get_array(file_data['data_type'], file_data['arr'])
+            arr_file_list.append((arr, json_file))
+
+        except ValueError as e:
+            print(f'fixture: get_array_list(): One of the attributes("data_type" or "arr")\
+                   is missing in the {json_file} file. Description: {e}')
+    
+    return arr_file_list
+
+def get_array(data_type: str, arr: list[Any]) -> list[Any]:
+    # ignoring data_type for now.
+    return arr
+
+def test_is_empty(get_array_list) -> None:
+    print('is_empty(): started...')
+
+    for arr, file_name in get_array_list:
+        if file_name == 'arr_01.json':
+            pass
+
+        elif file_name == 'arr_02.json':
+            pass
+
+    print('is_empty(): finished...')
