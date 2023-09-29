b83279f42fde1c79eb20b7bce105402fa2991783
Mohammadreza B <b.mohammadreza@gmail.com>
2023-09-29 14:38:13 -0400

    Refactor hashmap data structure implementation and implement its assiciated tests.

--- core_samples/hashmap/hashmap.py
+++ core_samples/hashmap/hashmap.py
@@ -6,33 +6,53 @@ class Hashmap:
     """ Simple hashmap implementation.
      Only accepts 'int' and 'str' as the key type. """
 
+    _BUCKET_SEARCH_GET = 0
+    _BUCKET_SEARCH_REMOVE = 1
+
     def __init__(self) -> None:
         self._index: int = 0
         self._hash_index_map: dict = {}
         self._hashmap: list[list[tuple]] = []
 
+    def is_valid_hash(self, hash) -> bool:
+        return hash in self._hash_index_map
 
-    def _bucket_search(self, entries: list[tuple], key) ->Any:
-        for entry in entries:
-            if entry[0] == key:
-                return entry[1]
-        else:
-            print(f'_bucket_search(): key->{key} not found')
-            raise KeyError
-
-
-    def _get_item(self, key) -> Any:
+    def _get_bucket(self, key) -> (int, list[tuple]):
         key_hash = hash(key)
 
         if key_hash not in self._hash_index_map:
-            print(f'_get_item(): key_hash->{key_hash} not found')
-            raise KeyError
+            raise KeyError(f'_get_bucket(): key_hash->{key_hash} not found')
         
         index = self._hash_index_map[key_hash]
-        entries = self._hashmap[index]
+        if index >= len(self._hashmap):
+            raise KeyError(f'_get_bucket(): index->{index} out of bound')
 
-        return self._bucket_search(entries, key)
+        return (index, self._hashmap[index])
 
+    def _bucket_search(self, op, entries_info: (int,list[tuple]), key) ->Any:
+        if op == self._BUCKET_SEARCH_GET:
+            return self._bucket_search_get(entries_info, key)
+
+        elif op == self._BUCKET_SEARCH_REMOVE:
+            return self._bucket_search_remove(entries_info, key)
+
+    def _bucket_search_get(self, entries_info: (int, list[tuple]), key) ->Any:
+        for entry in entries_info[1]:
+            if entry[0] == key:
+                return entry[1]
+        else:
+            raise KeyError(f'_bucket_search_get(): key->{key} not found')
+
+    def _bucket_search_remove(self, entries_info: (int, list[tuple]), key) ->Any:
+        for index, entry in enumerate(entries_info[1]):
+            if entry[0] == key:
+                entries_info[1].pop(index)
+                break
+        else:
+            raise KeyError(f'_bucket_search_remove(): key->{key} not found')
+
+    def _get_item(self, key) -> Any:
+        return self._bucket_search(op=self._BUCKET_SEARCH_GET, entries_info=self._get_bucket(key), key=key)
 
     def __getitem__(self, key: int) -> Any:
         """ Return the item of hashmap with key 'key'.
@@ -83,11 +103,18 @@ class Hashmap:
 
         self._add_item(key, value)
 
+    def _remove_item(self, key) -> None:
+        self._bucket_search(op=self._BUCKET_SEARCH_REMOVE, entries_info=self._get_bucket(key), key=key)
+
     def remove_item(self, key: int) -> None:
         """ Removes the entry with key 'key'. """
 
+        self._remove_item(key)
+
     def remove_item(self, key: str) -> None:
         """ Removes the entry with key 'key'. """
+
+        self._remove_item(key)
 
 if __name__ == "__main__":
     pass
--- core_samples/hashmap/test/test_hashmap.py
+++ core_samples/hashmap/test/test_hashmap.py
@@ -3,7 +3,7 @@ import pytest
 import json
 import os
 
-from typing import Self,Any
+from typing import Any
 
 @pytest.fixture
 def get_file_list() -> list[str]:
@@ -34,17 +34,133 @@ def get_array(data_type: str, arr: list[Any]) -> list[Any]:
     # ignoring data_type for now.
     return arr
 
-def test_get_item(get_array_list) -> None:
-    print('test_1(): started...')
+def test_add_item(get_array_list) -> None:
+    print('test_add_item(): started...')
 
     for arr, file_name in get_array_list:
         if file_name == 'arr_01.json':
-            pass
+            hash_map = hashmap.Hashmap()
+
+            for entry in arr:
+                hash_map.add_item(entry[0], entry[1])
+
+            assert hash_map[1] == "value_1"
+            assert hash_map[12] == "value_2"
+            assert hash_map["3"] == "value_3"
+            assert hash_map['25'] == "value_4"
+            assert hash_map[10] == "value_5"
 
         elif file_name == 'arr_02.json':
-            pass
+            hash_map = hashmap.Hashmap()
 
-        elif file_name == 'arr_03.json':
-            pass
+            for entry in arr:
+                hash_map.add_item(entry[0], entry[1])
 
-    print('test_1(): finished...')
+            assert hash_map[1100] == "value_1"
+            assert hash_map['abcd'] == "value_2"
+            assert hash_map[900245] == "value_3"
+
+    print('test_add_item(): finished...')
+
+def test_remove_item(get_array_list) -> None:
+    print('test_remove_item(): started...')
+
+    for arr, file_name in get_array_list:
+        if file_name == 'arr_01.json':
+            hash_map = hashmap.Hashmap()
+
+            for entry in arr:
+                hash_map.add_item(entry[0], entry[1])
+
+            assert hash_map[1] == "value_1"
+            assert hash_map[12] == "value_2"
+            assert hash_map["3"] == "value_3"
+            assert hash_map['25'] == "value_4"
+            assert hash_map[10] == "value_5"
+
+            hash_val = hash('25')
+            print(f'hash("25")->{hash_val}')
+            assert hash_map.is_valid_hash(hash=hash_val) == True
+            
+            hash_val = hash(12)
+            print(f'hash(12)->{hash_val}')
+            assert hash_map.is_valid_hash(hash=hash_val) == True
+
+            hash_map.remove_item('25')
+            assert hash_map.length() == 4
+            hash_map.remove_item(12)
+            assert hash_map.length() == 3
+
+            with pytest.raises(KeyError) as excep_info:
+                hash_map.remove_item('20')
+
+            hash_val = hash('20')
+            excp_str = str(excep_info.value)
+            print(excp_str)
+
+            if hash_map.is_valid_hash(hash=hash_val):
+                assert "key->" in excp_str
+            else:
+                assert "key_hash->" in excp_str
+
+            with pytest.raises(KeyError) as excep_info:
+                hash_map.remove_item('25')
+
+            hash_val = hash('25')
+            excp_str = str(excep_info.value)
+            print(excp_str)
+
+            if hash_map.is_valid_hash(hash=hash_val):
+                assert "key->" in excp_str
+            else:
+                assert "key_hash->" in excp_str
+
+        elif file_name == 'arr_02.json':
+            hash_map = hashmap.Hashmap()
+
+            for entry in arr:
+                hash_map.add_item(entry[0], entry[1])
+
+            assert hash_map[1100] == "value_1"
+            assert hash_map['abcd'] == "value_2"
+            assert hash_map[900245] == "value_3"
+
+            hash_val = hash('abcd')
+            print(f'hash("abcd")->{hash_val}')
+            assert hash_map.is_valid_hash(hash=hash_val) == True
+            
+            hash_val = hash(900245)
+            print(f'hash(900245)->{hash_val}')
+            assert hash_map.is_valid_hash(hash=hash_val) == True
+
+            hash_map.remove_item('abcd')
+            assert hash_map.length() == 2
+            hash_map.remove_item(900245)
+            assert hash_map.length() == 1
+
+            with pytest.raises(KeyError) as excep_info:
+                hash_map.remove_item(900245)
+
+            hash_val = hash(900245)
+            excp_str = str(excep_info.value)
+            print(excp_str)
+
+            if hash_map.is_valid_hash(hash=hash_val):
+                assert "key->" in excp_str
+            else:
+                assert "key_hash->" in excp_str
+
+            with pytest.raises(KeyError) as excep_info:
+                hash_map.remove_item('aaa')
+
+            hash_val = hash('aaa')
+            excp_str = str(excep_info.value)
+            print(excp_str)
+
+            if hash_map.is_valid_hash(hash=hash_val):
+                assert "key->" in excp_str
+            else:
+                assert "key_hash->" in excp_str
+
+
+    print('test_remove_item(): finished...')
